7ba057259e979f922a52433072e5b3c6f6df62fa
darwindev2002 <darwin0630@gmail.com>
2024-02-22 19:21:38 -0800

    ConsistentHashRing struct done

--- pkg/surfstore/ConsistentHashRing.go
+++ pkg/surfstore/ConsistentHashRing.go
@@ -3,6 +3,7 @@ package surfstore
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"sort"
 )
 
 type ConsistentHashRing struct {
@@ -10,16 +11,31 @@ type ConsistentHashRing struct {
 }
 
 func (c ConsistentHashRing) GetResponsibleServer(blockId string) string {
-	panic("todo")
+	serverHashes := make([]string, 0, len(c.ServerMap))
+	for k := range c.ServerMap {
+		serverHashes = append(serverHashes, k)
+	}
+	sort.Strings(serverHashes)
+	for _, serverHash := range serverHashes {
+		if serverHash > blockId {
+			return serverHash
+		}
+	}
+	return c.ServerMap[serverHashes[0]]
 }
 
 func (c ConsistentHashRing) Hash(addr string) string {
 	h := sha256.New()
 	h.Write([]byte(addr))
 	return hex.EncodeToString(h.Sum(nil))
-
 }
 
 func NewConsistentHashRing(serverAddrs []string) *ConsistentHashRing {
-	panic("todo")
+	hashRing := ConsistentHashRing{}
+	hashRing.ServerMap = make(map[string]string)
+	for _, serverAddr := range serverAddrs {
+		serverHash := hashRing.Hash("blockstore" + serverAddr)
+		hashRing.ServerMap[serverHash] = serverAddr
+	}
+	return &hashRing
 }
